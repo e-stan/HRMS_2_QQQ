2cf3ce452bbb4a5aecc534dca702444f99d225a9
e-stan <estancl1234@gmail.com>
2021-03-16 10:41:30 -0500

    tested install

--- analysis/example_script_multiple_files.py
+++ analysis/example_script_multiple_files.py
@@ -1,9 +1,9 @@
-from src.srm_helper import *
+from srm_helper import *
 import pandas as pd
 import numpy as np
 
 ### format csv files from whole transition list
-totalTransitionInfoFn = "data/IDX/M3T_transitions_ALTIS_optimized_allCpds.csv"
+totalTransitionInfoFn = "../data/IDX/M3T_transitions_ALTIS_optimized_allCpds.csv"
 
 totalTransitions = pd.read_csv(totalTransitionInfoFn)
 
@@ -13,7 +13,7 @@ totalTransitions["rt_start"] = np.array([.3 for _ in range(len(totalTransitions)
 totalTransitions["rt_end"] = np.array([1.0 for _ in range(len(totalTransitions))])
 
 filt = totalTransitions[totalTransitions["Training"] == 1]
-filt.to_csv("data/IDX/target_transitions_to_learn_conv.csv",index=False)
+filt.to_csv("../data/IDX/target_transitions_to_learn_conv.csv",index=False)
 
 targets = totalTransitions[totalTransitions["Training"] == 0]
 cpds = []
@@ -27,7 +27,7 @@ for index,row in targets.iterrows():
 targets = targets.drop(toDrop)
 goodCols = ["Name","rt_start","rt_end","mz","Charge"]
 targets = targets[goodCols]
-targets.to_csv("data/IDX/targets_for_evaluation.csv",index=False)
+targets.to_csv("../data/IDX/targets_for_evaluation.csv",index=False)
 
 if __name__ == "__main__":
     #params
@@ -40,13 +40,13 @@ if __name__ == "__main__":
 
     #set datafiles for learning conversion
     trainingData = pd.read_csv("../data/IDX/target_transitions_to_learn_conv.csv")
-    msFilenames = ["data/IDX/IDX_MS2_data/training_cpds/" + x for x in os.listdir(
+    msFilenames = ["../data/IDX/IDX_MS2_data/training_cpds/" + x for x in os.listdir(
         "../data/IDX/IDX_MS2_data/training_cpds/") if ".mzML" in x and "merged" not in x]
     print(msFilenames)
 
     #build conversion
     merged = srm_maker.buildConversion(msFilenames,trainingData,tic_cutoff=0,frag_cutoff=0,frag_ppm_tolerance=2 * 1e6 * .5/200)
-    merged.to_csv("data/IDX/target_transitions_to_learn_conv_optimizedValues.csv")
+    merged.to_csv("../data/IDX/target_transitions_to_learn_conv_optimizedValues.csv")
 
     #output conversion
     print(srm_maker.getConversionEquationString())
@@ -60,7 +60,7 @@ if __name__ == "__main__":
 
 
     #construct SRM tables for individual files
-    datadir = "data/IDX/IDX_MS2_data/"
+    datadir = "../data/IDX/IDX_MS2_data/"
     for index,row in targets.iterrows():
         if row["Charge"] > 0:
             polarity = "pos"
