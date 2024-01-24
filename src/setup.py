82b11750f64e3f6a6b1caa0edef75eb33817c182
e-stan <estancl1234@gmail.com>
2024-01-24 08:54:07 -0600

    changed scikitlearn

--- src/setup.py
+++ src/setup.py
@@ -9,11 +9,11 @@ setup(
   author = 'Ethan Stancliffe',                   # Type in your name
   author_email = 'estancl1234@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/e-stan/HRMS_2_QQQ',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/e-stan/HRMS_2_QQQ/archive/v0.0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/e-stan/HRMS_2_QQQ/archive/v0.0.2.tar.gz',    # I explain this later on
   keywords = ['Metabolomics', 'SRM', 'MS/MS',"QqQ"],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
-          'sklearn',
+          'scikit-learn',
           'pandas',
           'DecoID',
           'matplotlib'
